d5124af92a79f24e27821a0f8a35be66c559727e
anna-charlotte <charlotte.gerhaher@jina.ai>
2022-11-18 14:15:24 +0100

    fix: run ci without changes

--- executor.py
+++ executor.py
@@ -58,7 +58,6 @@ class QdrantIndexer(Executor):
                 'full_scan_threshold': full_scan_threshold,
                 'serialize_config': serialize_config or {},
                 'columns': columns,
-                'list_like': False,
             },
         )
 
@@ -85,7 +84,7 @@ class QdrantIndexer(Executor):
         :param kwargs: additional kwargs for the endpoint
 
         """
-        
+
         match_args = (
                 {**self._match_args, **parameters}
                 if parameters is not None
